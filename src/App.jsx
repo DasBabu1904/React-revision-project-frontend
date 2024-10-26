df6ddd2f98604caa81d17eafec11ea893eb0cb4e
DasBabu1904 <dass7550@gmail.com>
2024-10-26 14:59:24 +0600

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>This is new react app</h1>
+      <h2>Hello microphone testing </h2>
+    </div>
+  )
+}
+
+export default App;
