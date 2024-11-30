ed5ac7366bc0905d226762099be50a5e5ca93041
DasBabu1904 <dass7550@gmail.com>
2024-11-30 14:50:06 +0600

    enoployee dashboard updated

--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -3,18 +3,88 @@ import React from 'react'
 export const TaskList = () => {
   return (
     <div id="tasklist" className='flex overflow-x-auto item-center justify-start gap-5 flex-nowrap flex h-[55%] w-full py-5  mt-10'>
-        
-        <div className='flex-shrink-0 h-full w-[200px] p-5 bg-yellow-400 rounded-xl'>
-          <div className="flex justify-between items-center text-sm">
-            <h3 className="bg-red-600 px-3 py-1 rounded">
-              High
-            </h3>
-            <h4>20feb 2024</h4>
 
-          </div>
-
+      {/*------------------ Card for task ------------------------------------*/}
+      <div className='flex-shrink-0 h-full w-[270px] p-5 bg-red-400 rounded-xl'>
+        <div className="flex justify-between items-center text-sm">
+          <h3 className="bg-red-600 px-3 py-1 rounded">
+            High
+          </h3>
+          <h4>20feb 2024</h4>
         </div>
-        {/* <div className='flex-shrink-0 h-full w-[200px] bg-yellow-400 rounded-xl'>
+
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold text-left'>make a youtube video</h2>
+          <p className='text-sm mt-2 text-left'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dolor voluptatibus iure error culpa, quasi deserunt cumque, corrupti sint eveniet cum obcaecati minus a?
+          </p>
+        </div>
+      </div>
+      {/*------------------ Card for task ------------------------------------*/}
+      <div className='flex-shrink-0 h-full w-[270px] p-5 bg-yellow-400 rounded-xl'>
+        <div className="flex justify-between items-center text-sm">
+          <h3 className="bg-red-600 px-3 py-1 rounded">
+            High
+          </h3>
+          <h4>20feb 2024</h4>
+        </div>
+
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold text-left'>make a youtube video</h2>
+          <p className='text-sm mt-2 text-left'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dolor voluptatibus iure error culpa, quasi deserunt cumque, corrupti sint eveniet cum obcaecati minus a?
+          </p>
+        </div>
+      </div>
+      {/*------------------ Card for task ------------------------------------*/}
+      <div className='flex-shrink-0 h-full w-[270px] p-5 bg-blue-400 rounded-xl'>
+        <div className="flex justify-between items-center text-sm">
+          <h3 className="bg-red-600 px-3 py-1 rounded">
+            High
+          </h3>
+          <h4>20feb 2024</h4>
+        </div>
+
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold text-left'>make a youtube video</h2>
+          <p className='text-sm mt-2 text-left'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dolor voluptatibus iure error culpa, quasi deserunt cumque, corrupti sint eveniet cum obcaecati minus a?
+          </p>
+        </div>
+      </div>
+      {/*------------------ Card for task ------------------------------------*/}
+      <div className='flex-shrink-0 h-full w-[270px] p-5 bg-cyan-400 rounded-xl'>
+        <div className="flex justify-between items-center text-sm">
+          <h3 className="bg-red-600 px-3 py-1 rounded">
+            High
+          </h3>
+          <h4>20feb 2024</h4>
+        </div>
+
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold text-left'>make a youtube video</h2>
+          <p className='text-sm mt-2 text-left'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dolor voluptatibus iure error culpa, quasi deserunt cumque, corrupti sint eveniet cum obcaecati minus a?
+          </p>
+        </div>
+      </div>
+      {/*------------------ Card for task ------------------------------------*/}
+      <div className='flex-shrink-0 h-full w-[270px] p-5 bg-green-400 rounded-xl'>
+        <div className="flex justify-between items-center text-sm">
+          <h3 className="bg-red-600 px-3 py-1 rounded">
+            High
+          </h3>
+          <h4>20feb 2024</h4>
+        </div>
+
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold text-left'>make a youtube video</h2>
+          <p className='text-sm mt-2 text-left'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dolor voluptatibus iure error culpa, quasi deserunt cumque, corrupti sint eveniet cum obcaecati minus a?
+          </p>
+        </div>
+      </div>
+      {/* <div className='flex-shrink-0 h-full w-[200px] bg-yellow-400 rounded-xl'>
 
         </div>
         <div className='flex-shrink-0 h-full w-[200px] bg-yellow-400 rounded-xl'>
